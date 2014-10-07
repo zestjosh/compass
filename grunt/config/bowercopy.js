f8cbe19e3f7ec41bfafd1d72c07cdf2d18592cea
robneu <rob@wpsitecare.com>
2014-10-07 11:30:39 -0400

    Removed bourbon and neat from bower deps
    Because Bourbon and Neat must be included using their node variants, we
    no longer need these.

--- grunt/config/bowercopy.js
+++ grunt/config/bowercopy.js
@@ -7,8 +7,6 @@ module.exports = {
 			destPrefix: '<%= paths.bower %>'
 		},
 		files: {
-			bourbon: 'bourbon/dist',
-			neat: 'neat/app/assets/stylesheets',
 			normalize: 'normalize.css/normalize.css'
 		}
 	},
