3ff92747cfb4548d0b0f61896bc248ce61dfecab
robneu <rob@wpsitecare.com>
2014-10-08 08:19:09 -0400

    Removed unused options from grunt-sass config

---
+++ grunt/config/sass.js
@@ -0,0 +1,17 @@
+module.exports = {
+	options: {
+		sourceMap: true,
+		includePaths: require( 'node-neat' ).includePaths
+	},
+	theme: {
+		files: [
+			{
+				expand: true,
+				cwd: '<%= paths.authorAssets %>scss/',
+				src: '*.scss',
+				dest: '<%= paths.tmp %>',
+				ext: '.css'
+			}
+		]
+	}
+};
