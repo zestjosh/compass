1020980407c4fd09cf66ae8d546826cf2e191ef8
robneu <rob@wpsitecare.com>
2014-10-13 11:53:27 -0500

    Deprecated compass_footer_creds
    This function is no longer needed and will be removed in a future
    release.

--- theme/includes/general.php
+++ theme/includes/general.php
@@ -57,19 +57,11 @@ function compass_do_sticky_banner() {
 	<?php
 }
 
-add_action( 'wp_footer', 'compass_footer_creds' );
 /**
  * Display footer credits for the theme.
  *
- * @since   1.0.0
- * @return  void
+ * @since      1.0.0
+ * @return     void
+ * @deprecated This was moved into the footer.php template and will be deleted.
  */
-function compass_footer_creds() {
-	echo '<p class="credit">';
-		printf(
-			// Translators: 1 is current year, 2 is site name/link, 3 is WordPress name/link, and 4 is theme name/link.
-			__( 'Copyright &#169; %1$s %2$s. Powered by %3$s and %4$s.', 'compass' ),
-				date_i18n( 'Y' ), hybrid_get_site_link(), hybrid_get_wp_link(), hybrid_get_theme_link()
-			);
-	echo '</p><!-- .credit -->';
-}
+function compass_footer_creds() {}
