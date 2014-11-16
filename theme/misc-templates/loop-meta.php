8ea378a27f17ea9b90d24c2e9eec173427f58513
robneu <rob@wpsitecare.com>
2014-11-16 00:35:09 -0600

    Added a wrap element to the loop meta template
    This is useful in situations where you’re using a full-width layout on
    an archive page of some sort.

--- theme/misc-templates/loop-meta.php
+++ theme/misc-templates/loop-meta.php
@@ -13,22 +13,24 @@
 
 <div <?php hybrid_attr( 'loop-meta' ); ?>>
 
-	<h1 <?php hybrid_attr( 'loop-title' ); ?>><?php hybrid_loop_title(); ?></h1>
+	<div <?php hybrid_attr( 'wrap', 'loop-meta' ); ?>>
 
-	<?php hybrid_get_menu( 'breadcrumbs' ); ?>
+		<h1 <?php hybrid_attr( 'loop-title' ); ?>><?php hybrid_loop_title(); ?></h1>
 
-	<?php if ( is_category() || is_tax() ) : ?>
+		<?php if ( is_category() || is_tax() ) : ?>
 
-		<?php hybrid_get_menu( 'sub-terms' ); ?>
+			<?php hybrid_get_menu( 'sub-terms' ); ?>
 
-	<?php endif; ?>
+		<?php endif; ?>
 
-	<?php if ( ! is_paged() && $desc = hybrid_get_loop_description() ) : ?>
+		<?php if ( ! is_paged() && $desc = hybrid_get_loop_description() ) : ?>
 
-		<div <?php hybrid_attr( 'loop-description' ); ?>>
-			<?php echo $desc; ?>
-		</div><!-- .loop-description -->
+			<div <?php hybrid_attr( 'loop-description' ); ?>>
+				<?php echo $desc; ?>
+			</div><!-- .loop-description -->
 
-	<?php endif; ?>
+		<?php endif; ?>
+
+	</div>
 
 </div><!-- .loop-meta -->
