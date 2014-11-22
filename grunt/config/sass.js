8abf63efb51443e07d0d466d53b482ed0964faab
robneu <rob@wpsitecare.com>
2014-11-22 06:42:41 -0600

    Resolved conflicts

--- grunt/config/sass.js
+++ grunt/config/sass.js
@@ -1,33 +1,11 @@
-// https://github.com/gruntjs/grunt-sass
+// https://github.com/sindresorhus/grunt-sass
 module.exports = {
 	options: {
-<<<<<<< HEAD
+		includePaths: require( 'node-neat' ).includePaths,
 		sourceMap: true,
-		includePaths: require( 'node-neat' ).includePaths
-=======
-		force: true,
-		style: 'expanded',
-		trace: true,
-		lineNumbers: true
->>>>>>> develop
+		outputStyle: 'expanded'
 	},
 	theme: {
-		options: {
-			banner:
-				'/*!\n' +
-				'Theme Name:  <%= pkg.theme.name %>\n' +
-				'Version:     <%= pkg.version %>\n' +
-				'Author:      <%= pkg.theme.author %>\n' +
-				'Author URI:  <%= pkg.theme.authoruri %>\n' +
-				'Theme URI:   <%= pkg.theme.uri %>\n' +
-				'Description: <%= pkg.theme.description %>\n' +
-				'Tags:        <%= pkg.theme.tags %>\n' +
-				'Text Domain: <%= pkg.theme.textdomain %>\n' +
-				'Domain Path: <%= pkg.theme.domainpath %>\n' +
-				'License:     <%= pkg.theme.license %>\n' +
-				'License URI: <%= pkg.theme.licenseuri %>\n' +
-				'*/\n'
-		},
 		files: [
 			{
 				expand: true,
