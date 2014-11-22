ec1894b2eda82aa4a5efb4c0c2a3586b77ea346f
robneu <rob@wpsitecare.com>
2014-11-22 01:52:44 -0600

    Added checks for Flagship breadcrumb display

--- theme/menu/breadcrumbs.php
+++ theme/menu/breadcrumbs.php
@@ -10,19 +10,26 @@
  * @since       1.0.0
  */
 
-// Check for Yoast breadcrumb support.
-if ( function_exists( 'yoast_breadcrumb' ) ) {
-	yoast_breadcrumb( '<nav class="breadcrumbs" itemprop="breadcrumb">', '</nav>' );
-}
-
-// Check for hybrid breadcrumb support.
-else if ( function_exists( 'breadcrumb_trail' ) ) {
-	breadcrumb_trail(
-		array(
-			'container'     => 'nav',
-			'separator'     => '>',
-			'show_browse'   => false,
-			'show_on_front' => false,
-		)
-	);
+if ( function_exists( 'flagship_display_breadcrumbs' ) ) {
+	// Display breadcrumbs based on user selections in the customizer.
+	if ( flagship_display_breadcrumbs() ) {
+		// Use Yoast breadcrumbs if they're available.
+		if ( function_exists( 'yoast_breadcrumb' ) ) {
+			yoast_breadcrumb(
+				'<nav class="breadcrumbs" itemprop="breadcrumb">',
+				'</nav>'
+			);
+		}
+		// Fall back to Hybrid Core breadcrumbs if Yoast isn't available.
+		else {
+			breadcrumb_trail(
+				array(
+					'container'     => 'nav',
+					'separator'     => '//',
+					'show_browse'   => false,
+					'show_on_front' => false,
+				)
+			);
+		}
+	}
 }
